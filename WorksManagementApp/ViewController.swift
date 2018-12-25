38ecb5560956cc80fe0670298492f99728cb3c02
大原一倫 <oha31@yahoo.co.jp>
2018-12-25 15:16:21 +0900

    Initial Commit

---
+++ WorksManagementApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorksManagementApp
+//
+//  Created by 大原一倫 on 2018/12/25.
+//  Copyright © 2018 oharakazumasa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
